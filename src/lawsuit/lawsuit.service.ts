1e55294c2a75a0de27ca81b9e0ff417780e1fdcb
Julio Neto <nettojulio@hotmail.com>
2023-09-14 12:34:56 -0300

    (refact): refact service

--- src/lawsuit/lawsuit.service.ts
+++ src/lawsuit/lawsuit.service.ts
@@ -1,6 +1,6 @@
 import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import * as cheerio from 'cheerio';
-import { UrlDict, urlManagement } from '../utils/urlManagement';
+import { urlManagement } from '../utils/urlManagement';
 import { gatewayRequest } from './client/gateway';
 import {
   LawsuitMovesDTO,
@@ -8,69 +8,157 @@ import {
   LawsuitSidesDTO,
 } from './dto/lawsuit.dto';
 
+const regex = /[\n\t]/g;
+const spaceRegex = /[\u202F\u00A0]/g;
+
 @Injectable()
 export class LawsuitService {
   private readonly logger: Logger = new Logger(LawsuitService.name);
 
   async lawsuit(lawsuitNumber: string): Promise<LawsuitResponseDTO> {
-    this.logger.debug(`Starting requests`);
-    const urls = urlManagement(lawsuitNumber);
-    return this.lawsuitSerch(lawsuitNumber, urls);
+    try {
+      this.logger.debug(`Starting requests`);
+      const urls = urlManagement(lawsuitNumber);
+      const [firstInstanceRawHTML, secondInstanceRawHTMLCodes] =
+        await Promise.all([
+          gatewayRequest(
+            `${urls.showFirstInstanceUrl}?processo.foro=1&processo.numero=${lawsuitNumber}`,
+          ),
+          gatewayRequest(
+            `${urls.searchUrl}?cbPesquisa=NUMPROC&dePesquisaNuUnificado=${lawsuitNumber}&dePesquisaNuUnificado=UNIFICADO&tipoNuProcesso=UNIFICADO`,
+          ),
+        ]);
+
+      const secondInstanceLawsuitCodes = await this.extractLawsuitCodes(
+        secondInstanceRawHTMLCodes,
+      );
+
+      const secondInstanceRawHtmlStorage =
+        await this.extractSecondInstanceRawHTML(
+          secondInstanceLawsuitCodes,
+          `${urls.showSecondInstanceUrl}`,
+        );
+
+      const [primeiraInstancia, segundaInstancia] = await Promise.all([
+        this.extractFirstInstanceData(firstInstanceRawHTML),
+        this.extractSecondInstanceData(secondInstanceRawHtmlStorage),
+      ]);
+
+      return {
+        ...primeiraInstancia,
+        movimentacoes: {
+          primeiraInstancia: primeiraInstancia.movimentacoes.primeiraInstancia,
+          segundaInstancia,
+        },
+      };
+    } catch (error) {
+      throw new HttpException(error['message'], error['status']);
+    }
   }
 
-  private async lawsuitSerch(
-    lawsuitNumber: string,
-    urls: UrlDict,
+  private async extractFirstInstanceData(
+    rawHTML: string,
   ): Promise<LawsuitResponseDTO> {
-    const regex = /[\n\t]/g;
-    const spaceRegex = new RegExp(/[\u202F\u00A0]/g);
-    const firstInstancePromise = gatewayRequest(
-      `${urls.showFirstInstanceUrl}?processo.foro=1&processo.numero=${lawsuitNumber}`,
-    );
+    try {
+      const $ = cheerio.load(rawHTML);
 
-    const secondInstancePromise = gatewayRequest(
-      `${urls.searchUrl}?cbPesquisa=NUMPROC&dePesquisaNuUnificado=${lawsuitNumber}&dePesquisaNuUnificado=UNIFICADO&tipoNuProcesso=UNIFICADO`,
-    );
+      const errorMessageRawHTML = $('td#mensagemRetorno').text().trim();
+      if (errorMessageRawHTML) {
+        throw new HttpException(errorMessageRawHTML, HttpStatus.BAD_REQUEST);
+      }
 
-    const [firstInstanceRawHTML, secondInstanceRawHTMLCodes] =
-      await Promise.all([firstInstancePromise, secondInstancePromise]);
+      const classe = $('span#classeProcesso').text().trim();
+      const area = $('div#areaProcesso').text().trim();
+      const assunto = $('span#assuntoProcesso').text().trim();
+      const distribuicao = $('div#dataHoraDistribuicaoProcesso').text().trim();
+      const juiz = $('span#juizProcesso').text().trim();
+      const acao = $('div#valorAcaoProcesso')
+        .text()
+        .replace('R$', '')
+        .replaceAll('.', '')
+        .trim();
+      const [partes, primeiraInstancia] = await Promise.all([
+        this.extractPartes($),
+        this.extractMovimentacoes($, 1),
+      ]);
 
-    const secondInstanceLawsuitCodes = await this.extractLawsuitCodes(
-      secondInstanceRawHTMLCodes,
-    );
-
-    const segundaInstancia = await this.secondInstanceParse(
-      secondInstanceLawsuitCodes,
-      `${urls.showSecondInstanceUrl}?processo.codigo`,
-    );
-
-    const $ = cheerio.load(firstInstanceRawHTML);
-
-    const errorMessageRawHTML = $('td#mensagemRetorno').text().trim();
-
-    if (errorMessageRawHTML) {
-      throw new HttpException(errorMessageRawHTML, HttpStatus.BAD_REQUEST);
+      return {
+        classe,
+        area,
+        assunto,
+        distribuicao,
+        juiz,
+        acao,
+        partes,
+        movimentacoes: {
+          primeiraInstancia,
+          segundaInstancia: [],
+        },
+      };
+    } catch (error) {
+      throw new HttpException(error.message, error.status);
     }
+  }
 
-    const classe = $('span#classeProcesso').text().trim();
-    const area = $('div#areaProcesso').text().trim();
-    const assunto = $('span#assuntoProcesso').text().trim();
-    const distribuicao = $('div#dataHoraDistribuicaoProcesso').text().trim();
-    const juiz = $('span#juizProcesso').text().trim();
-    const acao = $('div#valorAcaoProcesso')
-      .text()
-      .replace('R$', '')
-      .replace('.', '')
-      .trim();
+  private async extractSecondInstanceRawHTML(
+    lawsuitCodes: Array<string>,
+    url: string,
+  ) {
+    try {
+      const rawHtmlStorage = lawsuitCodes.map(
+        async (code) => await gatewayRequest(`${url}?processo.codigo=${code}`),
+      );
+      return Promise.all(rawHtmlStorage);
+    } catch (error) {
+      throw new HttpException(error['message'], error['status']);
+    }
+  }
+
+  private async extractSecondInstanceData(
+    rawHtmlArray: Array<string>,
+  ): Promise<Array<LawsuitMovesDTO>> {
+    try {
+      const promises: Array<Promise<Array<LawsuitMovesDTO>>> = rawHtmlArray.map(
+        async (item) => {
+          const $ = cheerio.load(item);
+          const errorMessageRawHTML = $('td#mensagemRetorno').text().trim();
+          if (errorMessageRawHTML) {
+            throw new HttpException(
+              errorMessageRawHTML,
+              HttpStatus.BAD_REQUEST,
+            );
+          }
+          return this.extractMovimentacoes($, 2);
+        },
+      );
+      const resultArrays = await Promise.all(promises);
+      return resultArrays.reduce((acc, current) => [...acc, ...current], []);
+    } catch (error) {
+      throw new HttpException(error['message'], error['status']);
+    }
+  }
+
+  private async extractLawsuitCodes(rawHTML: string): Promise<Array<string>> {
+    const $ = cheerio.load(rawHTML);
+    const lawsuitSecondInstanceCodes: Array<string> = [];
+
+    $('input#processoSelecionado').each(function () {
+      lawsuitSecondInstanceCodes.push($(this).attr('value'));
+    });
+
+    return lawsuitSecondInstanceCodes;
+  }
+
+  private async extractPartes(
+    $: cheerio.CheerioAPI,
+  ): Promise<Array<LawsuitSidesDTO>> {
     const partes: Array<LawsuitSidesDTO> = [];
-    const primeiraInstancia: Array<LawsuitMovesDTO> = [];
+    const lawyerRegex = /\b(Advogad[oa]):?\s?\b/gi;
 
     $('table#tableTodasPartes')
       .children()
       .contents()
       .each(function () {
-        const lawyerRegex = new RegExp(/\b(Advogad[oa]):?\s?\b/gi);
-
         const participacao = $(this)
           .contents()
           .children('.tipoDeParticipacao')
@@ -111,135 +199,61 @@ export class LawsuitService {
         }
       });
 
+    return partes;
+  }
+
+  private async extractMovimentacoes(
+    $: cheerio.CheerioAPI,
+    instance: number,
+  ): Promise<Array<LawsuitMovesDTO>> {
+    const movimentacoes: Array<LawsuitMovesDTO> = [];
+
+    const selectors = {
+      dataSelector:
+        {
+          1: 'td.dataMovimentacao',
+          2: 'td.dataMovimentacaoProcesso',
+        }[instance] || '',
+      detalhesAndDescricaoSelector:
+        {
+          1: 'td.descricaoMovimentacao',
+          2: 'td.descricaoMovimentacaoProcesso',
+        }[instance] || '',
+    };
+
     $('tbody#tabelaTodasMovimentacoes')
       .children()
       .each(function () {
         const data = $(this)
-          .children('td.dataMovimentacao')
+          .children(`${selectors.dataSelector}`)
           .text()
           .replaceAll(regex, ' ')
-          .replaceAll(new RegExp(/[\u202F\u00A0]/g), ' ')
+          .replaceAll(spaceRegex, ' ')
           .trim();
 
         const detalhes = $(this)
-          .children('td.descricaoMovimentacao')
+          .children(`${selectors.detalhesAndDescricaoSelector}`)
           .contents()
           .closest('span')
           .text()
           .replaceAll(regex, ' ')
-          .replaceAll(new RegExp(/[\u202F\u00A0]/g), ' ')
+          .replaceAll(spaceRegex, ' ')
           .trim();
 
         const descricao = $(this)
-          .children('td.descricaoMovimentacao')
+          .children(`${selectors.detalhesAndDescricaoSelector}`)
           .text()
           .replaceAll(regex, ' ')
-          .replaceAll(new RegExp(/[\u202F\u00A0]/g), ' ')
+          .replaceAll(spaceRegex, ' ')
           .replace(detalhes, '')
           .trim();
 
-        const movimentacao: LawsuitMovesDTO = detalhes
+        const response = detalhes
           ? { data, descricao, detalhes }
           : { data, descricao };
 
-        primeiraInstancia.push(movimentacao);
+        movimentacoes.push(response);
       });
-
-    return {
-      classe,
-      area,
-      assunto,
-      distribuicao,
-      juiz,
-      acao,
-      partes,
-      movimentacoes: {
-        primeiraInstancia,
-        segundaInstancia,
-      },
-    };
-  }
-
-  /**
-   *
-   * @param rawHTML
-   * @returns
-   */
-  private async extractLawsuitCodes(rawHTML: string): Promise<Array<string>> {
-    const $ = cheerio.load(rawHTML);
-    const lawsuitSecondInstanceCodes: Array<string> = [];
-
-    $('input#processoSelecionado').each(function () {
-      lawsuitSecondInstanceCodes.push($(this).attr('value'));
-    });
-
-    return lawsuitSecondInstanceCodes;
-  }
-  /**
-   *
-   * @param lawsuitCodes
-   * @param url
-   * @returns
-   */
-  private async secondInstanceParse(
-    lawsuitCodes: Array<string>,
-    url: string,
-  ): Promise<Array<LawsuitMovesDTO>> {
-    const trimRegex = /[\n\t]/g;
-    const spaceRegex = /[\u202F\u00A0]/g;
-
-    const promises: Array<Promise<Array<LawsuitMovesDTO>>> = lawsuitCodes.map(
-      async (item) => {
-        const rawHTML = await gatewayRequest(`${url}=${item}`);
-        const $ = cheerio.load(rawHTML);
-
-        const movimentacoes: Array<LawsuitMovesDTO> = [];
-
-        $('tbody#tabelaTodasMovimentacoes')
-          .children()
-          .each(function () {
-            const data = $(this)
-              .children('td.dataMovimentacaoProcesso')
-              .text()
-              .replaceAll(trimRegex, ' ')
-              .replaceAll(spaceRegex, ' ')
-              .trim();
-
-            const detalhes = $(this)
-              .children('td.descricaoMovimentacaoProcesso')
-              .contents()
-              .closest('span')
-              .text()
-              .replaceAll(trimRegex, ' ')
-              .replaceAll(spaceRegex, ' ')
-              .trim();
-
-            const descricao = $(this)
-              .children('td.descricaoMovimentacaoProcesso')
-              .text()
-              .replaceAll(trimRegex, ' ')
-              .replaceAll(spaceRegex, ' ')
-              .replace(detalhes, '')
-              .trim();
-
-            const response = detalhes
-              ? { data, descricao, detalhes }
-              : { data, descricao };
-
-            movimentacoes.push(response);
-          });
-
-        return movimentacoes;
-      },
-    );
-
-    const resultArrays = await Promise.all(promises);
-
-    const movimentacoes: Array<LawsuitMovesDTO> = resultArrays.reduce(
-      (acc, current) => [...acc, ...current],
-      [],
-    );
-
     return movimentacoes;
   }
 }
